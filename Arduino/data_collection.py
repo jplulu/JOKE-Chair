0d468087d91a4aaa41b1cadcba1a2ce8325c034e
Omar Thenmalai <omar.thenmalai@gmail.com>
2021-04-08 16:51:19 -0400

    Updated arduino code

--- Arduino/data_collection.py
+++ Arduino/data_collection.py
@@ -7,8 +7,9 @@ NUM_SENSORS = 8
 
 
 def main():
-    ser = serial.Serial('COM29', 9600)
-    output = [['Reading 1', 'Reading 2', 'Reading 3', 'Reading 4', 'Reading 5', 'Reading 6', 'Reading 7', 'Reading 8']]
+    ser = serial.Serial('COM11', 9600)
+    output = [['Reading 1', 'Reading 2', 'Reading 3', 'Reading 4', 'Reading 5', 'Reading 6', 'Reading 7', 'Reading 8',
+               'Reading 9', 'Reading 10']]
     collect(ser, output)
 
 
